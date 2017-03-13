094cc342509c8e43358a4e5935c20a089fba4d07
xxxxaa <yanyi102506@gmail.com>
2017-03-13 11:24:56 +0800

    init project

---
+++ teamwork/teamwork-core/src/test/java/com/team/account/gen/MybatisGen.java
@@ -0,0 +1,28 @@
+package com.team.account.gen;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yan
+ * @Description:
+ * @date 2016/10/18
+ */
+public class MybatisGen {
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        InputStream inputStream = MybatisGen.class.getResourceAsStream("/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(inputStream);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
